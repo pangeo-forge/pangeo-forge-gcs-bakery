a30464b2436f8a84680b235fe62abd2a922e4381
Alex Bush <alexbush@developmentseed.org>
2021-08-09 13:31:56 +0100

    feat: add readme content; fix: rename nodepool

---
+++ README.md
@@ -0,0 +1,5 @@
+- Login with gcloud
+- terraform plan
+- Check plan
+- terraform apply
+- kubectl apply -f prefect.deployment.yaml
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "google_container_cluster" "primary" {
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
+  name       = "alex-bush-node-pool"
   location   = "us-central1"
   cluster    = google_container_cluster.primary.name
   node_count = 1
