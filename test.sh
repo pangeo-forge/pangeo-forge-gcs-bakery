f95e8891abb2d6fd830745e20aa94d18c9f88d62
Alex Bush <alexbush@developmentseed.org>
2021-08-10 14:29:50 +0100

    fix: adapt test code

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+FLOW_FILE=$1
+FULL_PATH=$(realpath $0)
+ROOT_PATH=$(dirname $FULL_PATH)
+docker run -it --rm \
+    -v $ROOT_PATH/test/$FLOW_FILE:/opt/$FLOW_FILE \
+    -e FLOW_STORAGE_CONNECTION_STRING \
+    -e FLOW_STORAGE_CONTAINER \
+    -e FLOW_CACHE_CONTAINER \
+    -e BAKERY_IMAGE \
+    -e PREFECT__CLOUD__AGENT__LABELS \
+    -e PREFECT_PROJECT \
+    -e PREFECT__CLOUD__AUTH_TOKEN \
+    $BAKERY_IMAGE python3 /opt/$FLOW_FILE
