7e9146870ce5a79b2ebff08683238f619c8a4797
Alex Bush <alexbush@developmentseed.org>
2021-09-14 14:22:06 +0100

    fix: auto add grafana source

--- scripts/get-grafana-admin.sh
+++ scripts/get-grafana-admin.sh
@@ -2,4 +2,4 @@
 echo "Grafana username is:"
 echo "admin"
 echo "Grafana password is:"
-kubectl get secret --namespace loki-stack loki-grafana -o jsonpath="{.data.admin-password}" | base64 -d ; echo
+kubectl get secret --namespace loki-stack loki-stack-grafana -o jsonpath="{.data.admin-password}" | base64 -d ; echo
